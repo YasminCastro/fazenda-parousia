4804591ef5e8e282b92e62d19c447c6108be6904
Yasmin <yasminsdcastro@gmail.com>
2024-07-02 14:33:27 -0300

    feat: cards data

--- app/api/cards/route.ts
+++ app/api/cards/route.ts
@@ -1,13 +1,86 @@
 import api from "@/lib/api";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
+    const url = new URL(request.nextUrl);
+    const batch = url.searchParams.get("batch");
+
+    if (!batch) throw new Error("Invalid batch");
+
     const { data } = await api.get("/data");
 
-    return Response.json(data);
+    let dashboardCards = [];
+
+    for (let card of dashboardCardsMapping) {
+      let newKPI = { ...card };
+      const foundKPI = data.find((kpi: any) => kpi.KPI === card.kpi);
+      if (foundKPI) {
+        newKPI.value = foundKPI[batch];
+      }
+
+      if (card.cardType === "double" && card.kpi2) {
+        const foundKPI2 = data.find((kpi: any) => kpi.KPI === card.kpi2);
+        if (foundKPI2) {
+          newKPI.value2 = foundKPI2[batch];
+        }
+      }
+      dashboardCards.push(newKPI);
+    }
+
+    return Response.json(dashboardCards);
   } catch (error: any) {
     return Response.json({ message: error.message }, { status: 500 });
   }
 }
+
+const dashboardCardsMapping: any = [
+  {
+    cardType: "simple",
+    title: "Receita do leite (R$)",
+    kpi: "Receita do Leite (R$)",
+  },
+  {
+    cardType: "double",
+    title: "Custo - Alimentação (R$)",
+    kpi: "Custo alimentação",
+    title2: "Custo/kg de leite",
+    kpi2: "",
+  },
+  {
+    cardType: "double",
+    title: "Margem sob alimentação (R$)",
+    kpi: "Margem sobre alimentação (R$/vaca/dia)",
+    title2: "Margem/kg de leite",
+    kpi2: "Margem sobre alimentação (R$/kg)",
+  },
+  {
+    cardType: "simple",
+    title: "Retorno sobre investimento (%)",
+    kpi: "Retorno Investimento (%)",
+  },
+  {
+    cardType: "simple",
+    title: "Produção do leite (kg)",
+    kpi: "Média de produção (kg/vaca/dia)",
+  },
+  {
+    cardType: "simple",
+    title: "Quantidade de animais",
+    kpi: "Número de Animais",
+  },
+  {
+    cardType: "simple",
+    title: "Eficiência alimentar",
+    kpi: "Eficiência Alimentar",
+  },
+  {
+    cardType: "double",
+    title: "Mastite",
+    kpi: "Vacas Mastite",
+    title2: "Carência",
+    kpi2: "Vacas Carência Mastite",
+  },
+];
--- components/Pages/Dashboard/Cards/index.tsx
+++ components/Pages/Dashboard/Cards/index.tsx
@@ -14,14 +14,25 @@ import {
   List,
 } from "lucide-react";
 import axios from "axios";
+import { useBatch } from "@/providers/BatchContext";
+
+interface ICard {
+  title: string;
+  value: string;
+  title2?: string;
+  value2?: string;
+  cardType: "simple" | "double";
+}
 
 export default function Cards() {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState([]);
+
+  const { selectedBatch } = useBatch();
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("/api/cards");
+        const response = await axios.get(`/api/cards?batch=${selectedBatch}`);
         setData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
@@ -35,62 +46,82 @@ export default function Cards() {
 
   return (
     <div className="mt-4 grid grid-cols-4 gap-4">
-      <SimpleCard
-        title="Receita do leite (R$)"
-        value="40,689"
-        icon={<DollarSign color="#8280ff" size={36} />}
-        color="#e4e4ff"
-        fromYesterday="8.5% abaixo de ontem"
-      />
-      <DoubleCard
-        title="Custo - Alimentação (R$)"
-        value="10293"
-        title2="Custo/kg de leite"
-        value2="1,20 - 45%"
-        icon={<ShoppingCart color="#fec53d" size={36} />}
-        color="#fef2d6"
-      />
-      <DoubleCard
-        title="Margem sob alimentação (R$)"
-        value="89,000"
-        title2="Margem/kg de leite"
-        value2="1,20 - 30%"
-        icon={<TrendingUp color="#4ad991" size={36} />}
-        color="#d9f7e7"
-      />
-      <SimpleCard
-        title="Retorno sobre investimento (%)"
-        value="97"
-        icon={<IterationCw color="#ff9871" size={36} />}
-        color="#ffded2"
-      />
-      <SimpleCard
-        title="Produção do leite (kg)"
-        value="40,689"
-        icon={<Milk color="#8280ff" size={36} />}
-        color="#e4e4ff"
-        fromYesterday="7% acima de ontem"
-      />
-      <SimpleCard
-        title="Quantidade de animais"
-        value="250"
-        icon={<List color="#fec53d" size={36} />}
-        color="#fef2d6"
-      />
-      <SimpleCard
-        title="Eficiência alimentar"
-        value="250"
-        icon={<Activity color="#4ad991" size={36} />}
-        color="#d9f7e7"
-      />
-      <DoubleCard
-        title="Mastite"
-        value="5 (2%)"
-        title2="Carência"
-        value2="1.2%"
-        icon={<BriefcaseMedical color="#ff9871" size={36} />}
-        color="#ffded2"
-      />
+      {data.map((card: ICard) => {
+        const { icon, color } = getIconAndColor(card.title);
+
+        if (card.cardType === "double" && card.title2 && card.value2) {
+          return (
+            <DoubleCard
+              title={card.title}
+              value={card.value}
+              title2={card.title2}
+              value2={card.value2}
+              icon={icon}
+              color={color}
+            />
+          );
+        }
+
+        return (
+          <SimpleCard
+            title={card.title}
+            value={card.value}
+            icon={icon}
+            color={color}
+          />
+        );
+      })}
     </div>
   );
 }
+
+const getIconAndColor = (title: string) => {
+  switch (title) {
+    case "Receita do leite (R$)":
+      return {
+        icon: <DollarSign color="#8280ff" size={36} />,
+        color: "#e4e4ff",
+      };
+    case "Custo - Alimentação (R$)":
+      return {
+        icon: <ShoppingCart color="#fec53d" size={36} />,
+        color: "#fef2d6",
+      };
+    case "Margem sob alimentação (R$)":
+      return {
+        icon: <TrendingUp color="#4ad991" size={36} />,
+        color: "#d9f7e7",
+      };
+    case "Retorno sobre investimento (%)":
+      return {
+        icon: <IterationCw color="#ff9871" size={36} />,
+        color: "#ffded2",
+      };
+    case "Produção do leite (kg)":
+      return {
+        icon: <Milk color="#8280ff" size={36} />,
+        color: "#e4e4ff",
+      };
+    case "Quantidade de animais":
+      return {
+        icon: <List color="#fec53d" size={36} />,
+        color: "#fef2d6",
+      };
+    case "Eficiência alimentar":
+      return {
+        icon: <Activity color="#4ad991" size={36} />,
+        color: "#d9f7e7",
+      };
+    case "Mastite":
+      return {
+        icon: <BriefcaseMedical color="#ff9871" size={36} />,
+        color: "#ffded2",
+      };
+
+    default:
+      return {
+        icon: <IterationCw color="#ff9871" size={36} />,
+        color: "#ffded2",
+      };
+  }
+};
