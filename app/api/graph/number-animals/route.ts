29276e7ca404e83164a5eee9967022f4b830ebf2
Yasmin <yasminsdcastro@gmail.com>
2024-07-12 15:51:23 -0300

    feat: pie chart

--- app/api/graph/number-animals/route.ts
+++ app/api/graph/number-animals/route.ts
@@ -1,17 +1,27 @@
 import api from "@/lib/api";
+import formatBatchName from "@/utils/formatBatchName";
+import { NextRequest } from "next/server";
 
 export const dynamic = "force-dynamic";
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
+    const url = new URL(request.nextUrl);
+    const batch = url.searchParams.get("batch");
+
     const { data } = await api.get("/quantidade-animais");
 
-    const array = Object.entries(data[0])
+    let array = Object.entries(data[0])
       .filter(([key, value]) => typeof value === "number" && key !== "Fazenda")
       .map(([key, value]) => ({
         key,
         value,
       }));
+
+    if (batch && batch !== "all") {
+      const key = formatBatchName(batch);
+      array = array.filter((item) => item.key === key);
+    }
 
     return Response.json(array);
   } catch (error: any) {
--- components/Pages/Dashboard/Graphs/NumberOfAnimals/index.tsx
+++ components/Pages/Dashboard/Graphs/NumberOfAnimals/index.tsx
@@ -1,22 +1,9 @@
 import { useFilterContext } from "@/providers/FilterContext";
-import formatBatchName from "@/utils/formatBatchName";
-import { formatXAxis } from "@/utils/formatXAxis";
-import { getBarColor, getBarColorByName } from "@/utils/getGraphColors";
+import { getBarColor } from "@/utils/getGraphColors";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import {
-  BarChart,
-  Bar,
-  Brush,
-  ReferenceLine,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts";
 
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from "recharts";
+import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from "recharts";
 
 export default function NumberOfAnimals() {
   const [data, setData] = useState([]);
@@ -29,7 +16,6 @@ export default function NumberOfAnimals() {
           `/api/graph/number-animals?batch=${selectedBatch}`,
         );
         setData(response.data);
-        console.log("data", data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -37,53 +23,6 @@ export default function NumberOfAnimals() {
 
     fetchData();
   }, [selectedBatch]);
-
-  // return (
-  //   <ResponsiveContainer width="100%" height="100%">
-  //     <BarChart
-  //       width={500}
-  //       height={300}
-  //       data={data}
-  //       margin={{
-  //         top: 5,
-  //         right: 30,
-  //         left: 20,
-  //         bottom: 5,
-  //       }}
-  //     >
-  //       <CartesianGrid strokeDasharray="3 3" />
-  //       <XAxis dataKey="date_record" tickFormatter={formatXAxis} />
-  //       <YAxis />
-  //       <Tooltip />
-  //       <Legend verticalAlign="top" wrapperStyle={{ lineHeight: "40px" }} />
-  //       <ReferenceLine y={0} stroke="#000" />
-  //       <Brush dataKey="date_record" height={30} stroke="#3b82f6" />
-  //       {selectedBatch !== "all" && (
-  //         <Bar
-  //           dataKey="value"
-  //           stackId="a"
-  //           name={formatBatchName(selectedBatch)}
-  //           fill={getBarColorByName(batches, selectedBatch)}
-  //         />
-  //       )}
-  //       {selectedBatch === "all" &&
-  //         batches.map((item, index) => {
-  //           if (item.value === "all") {
-  //             return;
-  //           }
-  //           return (
-  //             <Bar
-  //               dataKey={formatBatchName(item.value, true)}
-  //               stackId="a"
-  //               name={item.label}
-  //               fill={getBarColor(index)}
-  //               key={item.label}
-  //             />
-  //           );
-  //         })}
-  //     </BarChart>
-  //   </ResponsiveContainer>
-  // );
 
   return (
     <ResponsiveContainer width="100%" height="100%">
@@ -97,7 +36,6 @@ export default function NumberOfAnimals() {
           cy="50%"
           outerRadius={80}
           fill="#8884d8"
-          label
         >
           {data.map((entry, index) => (
             <Cell key={`cell-${index}`} fill={getBarColor(index)} />
