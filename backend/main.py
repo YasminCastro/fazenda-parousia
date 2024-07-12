a3f0e77b498d6f146e0bc4dd010108a7bf4758bf
Yasmin <yasminsdcastro@gmail.com>
2024-07-12 10:43:13 -0300

    feat: mockup backend

---
+++ backend/main.py
@@ -0,0 +1,35 @@
+from flask import Flask,  request, abort
+import pandas as pd
+import json
+
+app = Flask(__name__)
+@app.get('/data')
+def get_data():
+   df = pd.read_json('dados.json')
+   return df.to_json(orient='records')
+
+@app.get('/producao')
+def get_producao():
+   df = pd.read_json('producao.json')
+   df['Data'] = pd.to_datetime(df['Data'], format="%Y-%m-%dT%H:%M:%S.%f").dt.strftime('%Y-%m-%d')
+   df = df.sort_values('Data')
+   df.columns = ['data', 'producao']
+   return df.to_json(orient='records')
+
+@app.get('/mastite')
+def get_mastite():
+    df = pd.read_json('mastite.json')
+    df['Data'] = pd.to_datetime(df['date_record'], format="%Y-%m-%dT%H:%M:%S.%f").dt.strftime('%Y-%m-%d')
+    df =  df.sort_values('date_record')
+    return df.to_json(orient='records')
+
+
+@app.get('/receita-leite')
+def get_receita_leite():
+    df = pd.read_json('receita-leite.json')
+    df['date_record'] = pd.to_datetime(df['Data'], format="%Y-%m-%dT%H:%M:%S.%f").dt.strftime('%Y-%m-%d')
+    df =  df.sort_values('Data')
+    return df.to_json(orient='records')
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0",port=8001, threaded=True, debug=True)
---
+++ backend/readme.md
@@ -0,0 +1,25 @@
+# Endpoint
+* /data - Dados dos KPIs
+* /producao - Dados de produção
+* /mastite - Dados de mastite
+
+# Tabela
+ Dados do serviço produção ordenados pelo campo ordem
+
+# Gráficos
+## Aba Gráficos 
+Origem dos dados serviço /data
+
+1. Números animais -   Animais
+1. IOFC(R$/KG) - IoFC / litro
+1. IOFC(R$/VACA) - IoFC / vaca
+1. Dias em lactação - DEL
+1. Produção /Lote - Producao
+1. Produção Média - media_producao
+1. Eficiência Alimentar - Feed Efficiency
+1. Controle Leiteiro -  Controle Leiteiro
+
+## Aba Produção
+Origem do serviço /producao
+## Aba Mastite
+Origem do serviço / mastite
