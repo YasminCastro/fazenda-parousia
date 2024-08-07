ae2905428e0971c0e2a4634412df3c2bf7734b20
Yasmin <yasminsdcastro@gmail.com>
2024-08-07 11:00:35 -0300

    feat: number and percent

--- components/Pages/Dashboard/Graphs/NumberOfAnimals/PieGraph/index.tsx
+++ components/Pages/Dashboard/Graphs/NumberOfAnimals/PieGraph/index.tsx
@@ -13,12 +13,45 @@ export default function PieGraph({ data }: IProps) {
   const { batches } = useFilterContext();
 
   const totalValue = data.reduce((acc, entry) => acc + entry.value, 0);
+
   const renderCustomLabel = (entry: any) => {
-    let label = entry.value;
-    if (data.length !== 1) {
-      label = `${((entry.value / totalValue) * 100).toFixed(1)}%`;
-    }
-    return label;
+    const percentage = `${((entry.value / totalValue) * 100).toFixed(1)}%`;
+    const RADIAN = Math.PI / 180;
+    const radius =
+      entry.innerRadius + (entry.outerRadius - entry.innerRadius) * 0.5;
+    const x = entry.cx + radius * Math.cos(-entry.midAngle * RADIAN);
+    const y = entry.cy + radius * Math.sin(-entry.midAngle * RADIAN);
+
+    const outerRadius = entry.outerRadius * 1.2;
+    const xPercent =
+      entry.cx + outerRadius * Math.cos(-entry.midAngle * RADIAN);
+    const yPercent =
+      entry.cy + outerRadius * Math.sin(-entry.midAngle * RADIAN);
+
+    const fillColor = getBarColorByName(batches, entry.key);
+
+    return (
+      <>
+        <text
+          x={x}
+          y={y}
+          textAnchor="middle"
+          dominantBaseline="central"
+          fill="#000"
+        >
+          {entry.value}
+        </text>
+        <text
+          x={xPercent}
+          y={yPercent}
+          textAnchor="middle"
+          dominantBaseline="central"
+          fill={fillColor} // Usando a cor correspondente
+        >
+          {percentage}
+        </text>
+      </>
+    );
   };
 
   return (
@@ -31,6 +64,7 @@ export default function PieGraph({ data }: IProps) {
             cx="50%"
             cy="50%"
             label={renderCustomLabel}
+            labelLine={false}
             animationDuration={700}
           >
             {data.map((entry: any, index) => (
