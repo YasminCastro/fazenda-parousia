3687a4f5ae9c85cf2fd4af300fd1123a11224fbf
Yasmin <yasminsdcastro@gmail.com>
2024-08-13 10:36:33 -0300

    refactor; milk price

--- service/GetMilkPrice.ts
+++ service/GetMilkPrice.ts
@@ -1,9 +1,3 @@
-import { ICostValues } from "@/interfaces/Graphs/cost";
-import { IFoodEfficiency } from "@/interfaces/Graphs/foodEfficiency";
-import { IInvestmentReturn } from "@/interfaces/Graphs/investmentReturn";
-import { IMilkRevenue } from "@/interfaces/Graphs/milkRevenue";
-import formatLoteKeys from "@/utils/formatLoteKeys";
-
 // const example = [
 //   {
 //     date: "2024-03-30",
@@ -28,23 +22,6 @@ export default function MilkPrice(rawData: any, batch: string, date: any) {
   if (kpiFound) {
     return kpiFound[key];
   }
-
-  console.log(kpiDate);
-  console.log(kpiFound);
-
-  // for (let data of rawData) {
-  //   const kpiFound = data.data.find((kpi: any) => kpi.key === apiKey);
-
-  //   if (kpiFound) {
-  //     const date = data.date;
-  //     if (batch === "all") {
-  //       const batches = formatLoteKeys(kpiFound);
-  //       response.push({ date, ...batches });
-  //     } else {
-  //       response.push({ date, value: kpiFound[key] });
-  //     }
-  //   }
-  // }
 
   return 0;
 }
