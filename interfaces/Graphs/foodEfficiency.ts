9a6d79facb439e8349c89496420784c2310b3edf
Yasmin <yasminsdcastro@gmail.com>
2024-08-13 10:45:22 -0300

    refactor: title graphs

--- interfaces/Graphs/foodEfficiency.ts
+++ interfaces/Graphs/foodEfficiency.ts
@@ -1,11 +1,13 @@
 interface All {
   date: string;
+  title: string;
   fazenda: number;
   [lote: string]: number | string;
 }
 
 interface OneBatch {
   date: string;
+  title: string;
   value: number;
 }
 
