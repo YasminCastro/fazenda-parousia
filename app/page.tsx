a1182f074a5e9bb7671c4d5c302c6e98ed27f8db
Yasmin <yasminsdcastro@gmail.com>
2024-09-12 17:17:33 -0300

    refactor: dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
-import Dashboard from "@/components/Pages/Dashboard";
+import Cards from "@/components/Pages/Dashboard/Cards";
+import Charts from "@/components/Pages/Dashboard/Charts";
+import Header from "@/components/Pages/Dashboard/Header";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
-      <Dashboard />
+      <div className="m-8 space-y-5">
+        <Header />
+        <Cards />
+        <div className="h-96">
+          <Charts />
+        </div>
+      </div>
     </div>
   );
 }
