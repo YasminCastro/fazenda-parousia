0763a1763cb2ed53efc4b1b79a677e32e4400a45
Yasmin <yasminsdcastro@gmail.com>
2024-09-15 23:38:10 -0300

    refactor: animal info css

--- components/Pages/AnimalPerformance/AnimalInfo/index.tsx
+++ components/Pages/AnimalPerformance/AnimalInfo/index.tsx
@@ -1,10 +1,18 @@
 export default function AnimalInfo() {
+  const TextDetail = ({ title, description }: any) => {
+    return (
+      <div className="flex gap-1">
+        <p className="font-bold">{title}:</p>
+        <p>{description}</p>
+      </div>
+    );
+  };
+
   return (
-    <div className="flex gap-5">
-      <p>Lote:</p>
-      <p>Lactação:</p>
-      <p>DEL:</p>
-      <p>Produção:</p>
+    <div className="flex justify-center gap-5 text-3xl">
+      <TextDetail title="Lactação" description="2°" />
+      <TextDetail title="DEL" description="135" />
+      <TextDetail title="Produção" description="24kg" />
     </div>
   );
 }
